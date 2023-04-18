0c03ac16ddc4b33a56a76bf3a17e3b2ab463d94e
unknown <christkuzanuka@gmail.com>
2023-04-18 15:07:21 +0100

    le dossier views amélioré

--- views/login.php
+++ views/login.php
@@ -1,17 +1,23 @@
 <?php
+session_start();
+
 include '../apps/header.php'
 ?>
 <div class="container mt-2">
     <div class="row">
         <div class="col-md-3 bg-light"></div>
         <div class="col-md-9">
-            <form>
+            <h3 class="text-center">Connexion</h3>
+            <?php if(!empty($_SESSION['error'])):?>
+                <div class="alert alert-danger text-center"><?= $_SESSION['error']; session_destroy()?></div>
+            <?php endif ?>
+            <form action="../controllers/UserCtrl.php" method="post">
                 <div class="row m-2">
                     <div class="col-md-4">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
                     </div>
                     <div class="col-md-8">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="email" class="form-control" name="mail" placeholder="Adresse e-mail" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>   
                 </div>
                 <div class="row m-2">
@@ -19,11 +25,11 @@ include '../apps/header.php'
                         <label for="exampleInputPassword1" class="form-label">Password</label>
                     </div>
                     <div class="col-md-8">
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" class="form-control" name="password" placeholder="Mot de passe" id="exampleInputPassword1">
                     </div>   
                 </div>
                 <div class="d-flex justify-content-end ">
-                    <button type="submit" class="btn btn-primary mx-3">Submit</button>
+                    <button type="submit" name="login" class="btn btn-primary mx-3">Se connecter</button>
                 </div>
             </form>
         </div>
---
+++ views/show.php
@@ -0,0 +1,11 @@
+<?php
+include "../apps/header.php";
+?>
+<div class="container">
+    <div class="d-flex justify-content-center">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 bg-primary">.col-md-6 .offset-md-3</div>
+        </div>
+        <div class="bg-danger"><img src="https://z-m-static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="" width="112"></div>
+    </div>
+</div>
