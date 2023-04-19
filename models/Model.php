10cee09f30b7295d4ddd2c9f1122abb8ba20d047
unknown <christkuzanuka@gmail.com>
2023-04-19 16:46:23 +0100

    modification basée sur le model user

--- models/Model.php
+++ models/Model.php
@@ -1,9 +1,33 @@
 <?php
 namespace models;
+
+use PDO;
+
 class Model{
     public function get_pdo(){
-        $pdo = new  \PDO("mysql:host=localhost;dbname=facebook", "root", "");
+        $pdo = new  PDO("mysql:host=localhost;dbname=facebook", "root", "");
         return $pdo;
+    }
+    /**
+     * @param string $sql La requete sql 
+     * @param array $param Les paramètres à exécuter dans un tableau 
+     * @param bool $fetch Détermine le type de requete fetch par défaut Non 
+     * @param bool $one Détermine si on veut chercher un seul élément par defaut Non 
+     */
+    public function prepareQuery($sql, array $params, $fetch = false, $one = false ){
+        $query = $this->get_pdo()->prepare($sql);
+        $reponse = $query->execute($params);
+        if($fetch){
+            if($one){
+                $data = $query->fetch(PDO::FETCH_ASSOC);
+            }else{
+                $data = $query->fetchAll(PDO::FETCH_ASSOC);
+            }
+            if($data){
+                $reponse = $data;
+            }
+        }
+        return $reponse;
     }
     
 } 
--- models/UsersModel.php
+++ models/UsersModel.php
@@ -4,10 +4,27 @@ namespace models;
 require "Model.php";
 
 class UsersModel extends Model{
-    public function login($email, $password){
-        $query = $this->get_pdo()->prepare("SELECT * FROM users WHERE email = ? AND mdp = ?");
-        $query->execute([$email, $password]);
-        $data = $query->fetch(\PDO::FETCH_ASSOC);
+    public function loginUser($email, $password){
+        $sql = "SELECT * FROM users WHERE email = ? AND mdp = ?";
+        $param = [$email, $password];
+        $data = $this->prepareQuery($sql, $param, true, true);
+        return $data;
+    }
+    public function registerUser($lastname, $firstname, $birthdate, $email, $password, $picture=null){
+        $sql = "INSERT INTO users value (null, ?, ?,?, ?, ?, ?)";
+        $param = [$lastname, $firstname, $birthdate, $email, $password, $picture];
+        $query = $this->prepareQuery($sql, $param);
+        if($query){
+            return true;
+        }
+        return false;
+    }
+    public function all(){
+        $sql = "SELECT * FROM users";
+        $param = [];
+        $data = null;
+        $query = $this->prepareQuery($sql, $param, true);
+        $data = $query;
         return $data;
     }
 }
